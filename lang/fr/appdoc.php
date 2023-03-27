769ee9566b460aaf6e52956b88a649e28d6ad1b9
Charles <charles@notilac.fr>
2023-03-27 09:19:45 +0200

    MAJ LANG

--- lang/fr/appdoc.php
+++ lang/fr/appdoc.php
@@ -1,15 +1,18 @@
 <?php
+
 return [
-    'title_front' => 'Documentation',
-    'content' => 'Utilise une data source',
-    'description' => 'Description',
-    'form_name' => 'Modifier doc',
-    'limit_users' => 'Limité à certains rôles',
-    'menu_appdocs' => 'Documentations',
-    'menu_appdocs_description' => 'créer et modifier des pages de documentations',
-    'name' => 'Nom de la page',
-    'no_doc' => 'Aucune doc disponible',
-    'roles' => 'Rôles',
-    'slug' => 'Slug page',
-    'title' => 'Gestion documentations'
+    "content" => "Utilise une data source",
+    "create" => "Créer Doc",
+    "description" => "Description",
+    "form_name" => "Modifier doc",
+    "limit_users" => "Limité à certains rôles",
+    "menu_appdocs" => "Documentations",
+    "menu_appdocs_description" => "créer et modifier des pages de documentations",
+    "name" => "Nom de la page",
+    "preview_name" => "Preview Doc",
+    "roles" => "Rôles",
+    "slug" => "Slug page",
+    "title" => "Gestion documentations",
+    "title_front" => "Documentation",
+    "update" => "MAJ Doc"
 ];
