cdde21614c1f9cbf73856152b2998c4d92d48c13
Charles <charles@notilac.fr>
2023-04-16 15:08:59 +0200

    MAJ LANG

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,5 +3,6 @@
 return [
     'app_usage' => 'Utilisation de l\'app',
     'documentation' => 'Documentation',
-    'text_intro_controller' => 'Cliquez sur une fiche documentaire dans le menu de gauche.'
+    'text_intro_controller' => 'Cliquez sur une fiche documentaire dans le menu de gauche.',
+    'technical_doc' => 'Doc technique'
 ];
